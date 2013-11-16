08e6acf6406a283e0f0857cb3faf9c17476f0551
e-cooper <elicooper18@gmail.com>
2013-11-16 15:05:44 -0500

    Fix load btn and added test package

---
+++ Anonymule/src/com/cs2340/anonymule/Tests/tests.java
@@ -0,0 +1,14 @@
+package com.cs2340.anonymule.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
